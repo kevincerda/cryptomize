fd569fc9e568afbedf2dd1bc828a41cae14c1867
Kevin <kevin@cerda.io>
2018-12-08 12:45:06 -0800

    Wrapped chart within a container for styling purposes.

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -7,10 +7,10 @@ export default class Header extends Component {
   }
   render() {
     return (
-      <header class="jumbotron jumbotron-fluid container" id="header">
-        <div class="container">
-          <h1 class="display-4">Welcome to Cryptomize</h1>
-          <p class="lead">
+      <header className="jumbotron jumbotron-fluid container" id="header">
+        <div className="container">
+          <h1 className="display-4">Welcome to Cryptomize</h1>
+          <p className="lead">
             This is a modified jumbotron that occupies the entire horizontal
             space of its parent.
           </p>
