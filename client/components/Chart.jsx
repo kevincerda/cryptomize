f0ecd700524e8d4df3276c7eafeafc0cd3b1cf1f
Kevin <kevin@cerda.io>
2018-12-08 11:36:55 -0800

    Chart rendering with demo data.

--- client/components/Chart.jsx
+++ client/components/Chart.jsx
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import Chart from 'chart.js';
 
-export default class Chart extends Component {
+export default class ResultsChart extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      chartConfig: {
+        data: {
+          labels: [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July'
+          ],
+          datasets: [
+            {
+              label: 'My First dataset',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [0, 10, 5, 2, 20, 30, 45]
+            }
+          ]
+        },
+        type: 'line',
+        options: {}
+      }
+    };
+    this.buildChart = this.buildChart.bind(this);
+  }
+
+  componentDidMount() {
+    this.buildChart(this.state.chartConfig);
+  }
+
+  buildChart(options) {
+    const canvas = findDOMNode(this.refs.chart);
+    const ctx = canvas.getContext('2d');
+    let chart = new Chart(ctx, options);
   }
 
   render() {
@@ -11,7 +48,7 @@ export default class Chart extends Component {
       <section className="container" id="chart">
         <div className="row">
           <div className="col-12">
-            <h3>Chart</h3>
+            <canvas ref="chart" />
           </div>
         </div>
       </section>
