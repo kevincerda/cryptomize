671161d54f29dbfa88690d633ed106cc466723fd
Kevin <kevin@cerda.io>
2018-12-08 10:25:52 -0800

    Switched to import bootstrap minified css file instead.

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import '../public/main.css';
-import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App.jsx';
 
 render(<App />, document.getElementById('root'));
