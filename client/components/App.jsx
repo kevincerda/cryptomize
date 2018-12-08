92701169093a903b1d399798247054fe3ba20523
Kevin <kevin@cerda.io>
2018-12-08 10:47:30 -0800

    Import chart component.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
+import Chart from './Chart.jsx';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Chart />
       </div>
     );
   }
