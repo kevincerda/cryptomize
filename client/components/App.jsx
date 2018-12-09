e9ec10b7889ec64fecf0cc9d63dea40692d06d1f
Kevin <kevin@cerda.io>
2018-12-08 18:55:22 -0800

    Moved chart logic into chart component, component now rendering data fetched from api on chartjs line graph.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.jsx';
 import ResultsChart from './Chart.jsx';
+import axios from 'axios';
 
 export default class App extends Component {
   render() {
