8fb40d681200c5f8ad74bcb62d078c7529846635
Kevin <kevin@cerda.io>
2018-12-10 14:35:01 -0800

    Clean up.

--- client/services/getHistoricalData.js
+++ client/services/getHistoricalData.js
@@ -7,8 +7,3 @@ const getHistoricalData = (start, end) => {
 };
 
 export default getHistoricalData;
-
-// ?start=<VALUE>&end=<VALUE> Allows data to be returned for a specific date range.
-// ?start=${props.start}&end=${props.end}
-//Must be listed as a pair of start and end parameters, with dates supplied in the YYYY-MM-DD format, e.g. 2013-09-01 for September 1st, 2013.
-// ?for=yesterdaySpecifying this will return a single value for the previous day. Overrides the start/end parameter.
